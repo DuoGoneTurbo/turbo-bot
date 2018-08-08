9e9c0271aa50adb0777f91703f37858bc35fb515
DuoGoneTurbo <42210464+DuoGoneTurbo@users.noreply.github.com>
2018-08-08 10:13:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const prefix = "!";
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+​
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
 ​
 // Set the prefix
 let prefix = "!";
