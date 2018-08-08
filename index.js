c20d0be872bf52b16e24194b06843debd15b6bcc
DuoGoneTurbo <42210464+DuoGoneTurbo@users.noreply.github.com>
2018-08-08 10:05:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ client.on("ready", () => {
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
+  } else
+​
+  if (message.content.startsWith("foo")) {
+    message.channel.send("bar!");
   }
 });
 ​
