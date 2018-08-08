3d68908f856f58d83cea995a3529dbdefd683604
DuoGoneTurbo <42210464+DuoGoneTurbo@users.noreply.github.com>
2018-08-08 09:31:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,13 +11,18 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = '-';
+  let prefix = '!';
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
   if (cmd === `${prefix}ping`){
     message.channel.send("Pong!");
+  }
+});
+
+if (cmd === `${prefix}Hello`){
+    message.channel.send("Hi there :wave:");
   }
 });
 
