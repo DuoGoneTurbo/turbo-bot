c511c010dd982e75335e7530af7e7650b5fc3c4d
DuoGoneTurbo <42210464+DuoGoneTurbo@users.noreply.github.com>
2018-08-08 09:02:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,23 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
+if(command === "purge") {
+    // This command removes all messages from all users in the channel, up to 100.
+    
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+});
+
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity(`Hey, Wassup!`);
