f507f0db04537651460441144c3610ea78f43fa0
DuoGoneTurbo <42210464+DuoGoneTurbo@users.noreply.github.com>
2018-08-08 16:26:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ if(command === "purge") {
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
     // So we get our messages, and delete them. Simple enough, right?
-    const fetched = message.channel.fetchMessages({limit: deleteCount});
+async function start() {
+  const fetched = message.channel.fetchMessages({limit: deleteCount});
+}
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
