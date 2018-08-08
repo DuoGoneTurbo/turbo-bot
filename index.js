cee012deb21823b76392416a27265063512ab105
DuoGoneTurbo <42210464+DuoGoneTurbo@users.noreply.github.com>
2018-08-08 10:58:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
+  if(message.content === "ayy") {
+    message.channel.send("Ayy, lmao!");
+  }
+  if(message.content === "wat") {
+    message.channel.send("Say what?");
+  }
+  if(message.content === "lol") {
+    message.channel.send("roflmaotntpmp");
+  }
+});
+
+client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
   }
