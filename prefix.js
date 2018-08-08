f8f380058ba954f22bb14d53c833502d45c0de65
DuoGoneTurbo <42210464+DuoGoneTurbo@users.noreply.github.com>
2018-08-08 10:08:34 -0500

    Delete prefix.js

--- prefix.js
+++
@@ -1,7 +0,0 @@
-// Set the prefix
-const prefix = "!";
-client.on("message", (message) => {
-  // our new check:
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-  // [rest of the code]
-});
