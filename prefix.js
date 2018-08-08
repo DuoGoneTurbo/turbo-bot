a5532a84a92cb1c451e398e0af89ca07b0fcd557
DuoGoneTurbo <42210464+DuoGoneTurbo@users.noreply.github.com>
2018-08-08 10:07:45 -0500

    Update prefix.js

--- prefix.js
+++ prefix.js
@@ -1,13 +1,7 @@
 // Set the prefix
 const prefix = "!";
 client.on("message", (message) => {
-  // Exit and stop if it's not there
-  if (!message.content.startsWith(prefix)) return;
-​
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "foo")) {
-    message.channel.send("bar!");
-  }
+  // our new check:
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  // [rest of the code]
 });
